d667c59c1e06436c8712149a50faa5f6c967d64d
rashmika6 <rashmikapramod115@gmail.com>
2024-12-13 16:08:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HU0giA6Y#w9kkDzlJsEFiVdvdxTZYBaJur_k7EzrfAyUdGl6IqOw", /* you sessin_id */
+SESSION_ID: process.env.SESSION_ID || "jFsUFaaB#2zXrriUWVn9Jbyg-axIVl6ZRvXRRFPk887Zc0pGt3Es", /* you sessin_id */
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.fm/u/44nfrrb8xk",
-ALIVE_MSG: process.env.ALIVE_MSG || "ඔව් rash තමයි කියන්න අපෙ බොට්ලට ඔය එක බොටෙක්වත් අදාල නැ මහත්තයෝ 😂🚫🌐🔥",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/bXdfVZ5/1a4ad18f278c9d26.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "ඔව් rash තමයි කියන්න අපෙ බොට්ලට ඔය එක බොටෙක්වත් අදාල නැ මහත්තයෝ 😂🚫🌐🔥+94742749398",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
